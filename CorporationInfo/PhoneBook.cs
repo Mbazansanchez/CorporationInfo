6ca7689582829607325d491e35be73ca3a9a32eb
MICHAEL BAZAN SANCHEZ <mbazansanchez@cnm.edu>
2021-08-06 13:04:58 -0600

    Add project files.

---
+++ CorporationInfo/PhoneBook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+    sealed class PhoneBook
+    {
+        public List<PhoneBookItem> PhoneBookEntries { get; set; }
+
+        public PhoneBook()
+        {
+            PhoneBookEntries = new List<PhoneBookItem>();
+        }
+        public string GetPhoneBook()
+        {
+            string phonebook = "";
+            // MB changed from string item to var item
+            foreach (var item in PhoneBookEntries)
+            {
+                phonebook += item.GetContactSummary() + "\n\n";
+            }
+            return phonebook;
+        }
+
+    }
+}
---
+++ CorporationInfo/PhoneBookItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+    //MB needed to change class to interface
+    interface PhoneBookItem
+    {
+        //MB removed public 
+        string GetContactSummary();
+    }
+}
